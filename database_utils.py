14f90522fd5df57479a946c4527c056d5240f96e
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-03-10 21:29:16 +0000

    Creates a method to intialise and return the db_engine

--- database_utils.py
+++ database_utils.py
@@ -1,13 +1,27 @@
 import yaml
 from yaml.loader import SafeLoader
+from sqlalchemy import create_engine, MetaData, inspect
 
 class DatabaseConnector:
+
+    def __init__(self, file_path="E:\Studies\AiCore\Project_4_Multinational_Data\Project_Files\multinational-retail-data-centralisation\db_creds.yaml"):
+        self.file_path = file_path
     
-    @staticmethod
-    def read_db_creds():
-        file_path = "db_creds.yaml"
 
-        with open(file_path) as f:
-            cred_dict = yaml.load(f, loader=SafeLoader)
+    def read_db_creds(self):
+        with open(self.file_path) as f:
+            db_cred_dict = yaml.load(f, loader=SafeLoader)
 
-        return cred_dict
+        return db_cred_dict
+
+
+    def init_db_engine(self):
+        #getting the dictionary credentials
+        db_cred_dict = self.read_db_creds()
+
+        #forming the database URL
+        db_url = f'postgresql://{db_cred_dict["RDS_USER"]}:{db_cred_dict["RDS_PASSWORD"]}@{db_cred_dict["RDS_HOST"]}:{db_cred_dict["RDS_PORT"]}/{db_cred_dict["RDS_DATABASE"]}'
+
+        #creating the engine
+        db_engine = create_engine(db_url)
+        return db_engine
