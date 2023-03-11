2d34372d003c9cb62e44288c8c816ba09a4a9573
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-03-11 11:48:06 +0000

    Adds the read_rds_table() method

--- data_extraction.py
+++ data_extraction.py
@@ -1,4 +1,18 @@
+import pandas as pd
+import yaml
+from yaml.loader import SafeLoader
+from sqlalchemy import create_engine, MetaData, inspect, text
+
 from database_utils import DatabaseConnector
 
 class DataExtractor:
-    pass
+    
+    @staticmethod
+    def read_rds_table(db_connc, table_name):
+        db_engine = db_connc.init_db_engine()
+        query = f'SELECT * FROM {table_name}'
+        
+        with db_engine.begin() as conn:
+            users_df = pd.read_sql_query(sql=text(query), con=conn)
+
+        return users_df
