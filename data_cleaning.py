7f4a01af8431b8327ce67d6d25d34399b05c9c6b
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-03-18 11:25:22 +0000

    Adds clean_products_data() and convert_products_weights() methods in DataCleaning class to clean the products dataframe

--- data_cleaning.py
+++ data_cleaning.py
@@ -194,7 +194,142 @@ class DataCleaning:
         return stores_df
 
 
+    @staticmethod
+    def clean_products_data(products_df):
+        print("Checking for NaN values:")
+        print("-------------------------")
+
+        for col in products_df.columns:
+            print(f" Number of rows removed for NaN values in column {col}: {products_df[col].isna().sum()}")
+            products_df.dropna(subset=[col], inplace=True)
+        print('\n')
+
+        print("Checking for Non-Numeric values in numeric columns:")
+        print("-----------------------------------------------------")
+
+        products_df_col = pd.to_numeric(products_df['EAN'], errors='coerce')
+        products_df_col_series = pd.Series(products_df_col.isna())
+
+        indices = products_df_col_series[products_df_col_series].index.tolist()
+        indices_index = products_df.loc[indices, 'Unnamed: 0'].tolist()
+
+        print(indices_index)
+
+        for index_num in indices_index:
+            products_df = products_df.drop(products_df.loc[products_df['Unnamed: 0'] == index_num].index)
+
+        print(f"Number of rows dropped for invlaid values in column 'EAN'  : {len(indices_index)}")
+        print('\n')
+
+        print("Checking for Invalid Dates:")
+        print("-----------------------------")
+
+        products_df_date_format = pd.to_datetime(products_df['date_added'], format='%Y-%m-%d', errors='coerce')
+        products_df_date_format_series = pd.Series(products_df_date_format.isna())
+
+        indices = products_df_date_format_series[products_df_date_format_series].index.tolist()
+        indices_index = products_df.loc[indices, 'Unnamed: 0'].tolist()
+
+        #print(indices_index)
+
+        for index in indices_index:
+                #print(stores_df.loc[stores_df['index'] == num, 'opening_date'])
+                values_split = products_df.loc[products_df['Unnamed: 0'] == index, 'date_added'].iloc[0].split(' ')
+
+                for value in values_split:
+                    if len(value) == 2:
+                        day_value = value
+                    elif len(value) == 4 and value.isnumeric() == True:
+                        year_value = value
+                    else:
+                        month_value = value
+
+
+                month_number = '{:02d}'.format(datetime.datetime.strptime(month_value, '%B').month)
+                final_date = year_value + '-' + str(month_number) + '-' + day_value
+
+                print(f"Changing {products_df.loc[products_df['Unnamed: 0'] == index, 'date_added'].iloc[0]} to {final_date}")
+                products_df.loc[products_df['Unnamed: 0'] == index, 'date_added'] = final_date
+
+
+        print('\n')
+        
+        return products_df
     
+
+    @staticmethod
+    def isfloat(value):
+        try:
+            float(value)
+            return True
+        except ValueError:
+            return False
+    
+
+    @staticmethod
+    def convert_product_weights(products_df):
+        products_df = DataCleaning.clean_products_data(products_df)
+        print("Checking for Weights column:")
+        print("-----------------------------")
+
+        count = 0
+        for i in products_df.index.tolist():
+            unit = []
+            ind = -1
+            #print(f"{i} -> {products_df.loc[i, 'weight']}")
+
+            while products_df.loc[i, 'weight'][ind].isalpha() == True:
+                unit = [products_df.loc[0, 'weight'][ind]] + unit
+                ind -= 1
+
+            unit = ''.join(unit)
+            #print(unit)
+
+            if DataCleaning.isfloat(products_df.loc[i, 'weight'][:(len(products_df.loc[i, 'weight'])+ind+1)]) == False:
+
+                if " x " in products_df.loc[i, 'weight'][:(len(products_df.loc[i, 'weight'])+ind+1)]:
+                    value = products_df.loc[i, 'weight'][:(len(products_df.loc[i, 'weight'])+ind+1)]    #{unit}")
+                    #print(value)
+
+                    value = value.split(' x ')
+                    #print(value)
+
+                    value_new = str((float(value[0]) * float(value[1]))/1000) + 'kg'
+                    #print(f"{value} -> {value_new}")
+
+                    products_df.loc[i, 'weight'] = value_new
+                    count += 1
+
+                else:
+                    #print(f"index {i} -> {products_df.loc[i, 'weight'][:(len(products_df.loc[i, 'weight'])+ind+1)]} {unit}")
+                    value = products_df.loc[i,'weight'][:2]
+                    value_new = str(float(value)/1000) + 'kg'
+
+                    #print(value_new)
+                    products_df.loc[i, 'weight'] = value_new
+                    count += 1
+
+                    
+            elif unit == 'kg':    
+                value = float(products_df.loc[i, 'weight'][:(len(products_df.loc[i, 'weight'])+ind+1)])
+                #print(f"{value} {unit}")
+
+            elif unit == 'g' or unit == 'ml':
+                value = float(products_df.loc[i, 'weight'][:(len(products_df.loc[i, 'weight'])+ind+1)])
+                value_new = str(value/1000) + 'kg'
+                #print(f"{value} {unit} -> {value_new} kg")
+
+                products_df.loc[i, 'weight'] = value_new
+                count += 1
+
+            else:
+                print(f"index: {i}  has unit: {unit}")
+
+        print(f"Converted '{count}' weight rows into units of kg")
+
+        return products_df
+
+
     
 
 #Testing the code
